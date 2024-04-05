76a2147a253fcdd48fc6f91d0c6fe9ddc5da1387
Fernando Herrera <fernando.herrera85@gmail.com>
2024-04-05 14:50:04 -0400

    Refactor MessageBox.test.ts

--- tests/components/chat/MessageBox.test.ts
+++ tests/components/chat/MessageBox.test.ts
@@ -2,12 +2,44 @@ import MessageBox from '@/components/chat/MessageBox.vue';
 import { mount } from '@vue/test-utils';
 
 describe('<MessageBox />', () => {
-  test('renders input and button elements correctly', () => {
-    const wrapper = mount(MessageBox);
+  const wrapper = mount(MessageBox);
 
+  test('renders input and button elements correctly', () => {
     expect(wrapper.html()).toMatchSnapshot();
     expect(wrapper.find('input[type="text"]').exists()).toBe(true);
     expect(wrapper.find('button').exists()).toBe(true);
     expect(wrapper.find('button svg').exists()).toBe(true);
+  });
+
+  test('emits sendMessage event when button is clicked with message value', async () => {
+    const message = 'Hola Mundo';
+
+    await wrapper.find('input[type="text"]').setValue(message);
+    await wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted('sendMessage')?.[0]).toEqual([message]);
+
+    expect((wrapper.vm as any).message).toBe('');
+  });
+
+  test('emits sendMessage event when keypress.enter is triggered with message value', async () => {
+    const message = 'Hola Mundo';
+
+    const input = wrapper.find('input');
+    await input.setValue(message);
+    await input.trigger('keypress.enter');
+
+    expect(wrapper.emitted('sendMessage')?.[0]).toEqual([message]);
+  });
+
+  test('emits sendMessage event when keypress.enter is triggered with message value', async () => {
+    const wrapper = mount(MessageBox);
+
+    const input = wrapper.find('input');
+
+    await input.trigger('keypress.enter');
+    await wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted('sendMessage')).toBeFalsy();
   });
 });
